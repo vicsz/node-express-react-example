c61d610e99f98929c79311fc7ab4f7f0f0cd27a3
Victor Szoltysek <victorszoltysek@Victors-MacBook-Pro.local>
2016-09-17 08:53:21 -0400

    Adding static content folder.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+//Serve static content from public folder
+app.use(express.static('public'));
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
